df25d7dbf413bbc5b8ba867d197718a07332d8b7
QA Murat Tas <itmmt38@gmail.com>
2022-12-15 22:13:10 +0300

    selenium

--- src/test/java/Day02_webElementLocators/C01_Locators.java
+++ src/test/java/Day02_webElementLocators/C01_Locators.java
@@ -11,22 +11,33 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class C01_Locators {
     WebDriver driver;
+
     @Before
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-         driver = new ChromeDriver();
-         driver.get("https://www.linkedin.com/");
+        driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
     }
+
     @After
-    public void tearDown()  {
+    public void tearDown() {
         //driver.quit();
     }
 
     @Test
     public void idLocator() {
         // id locator "session_key"
-       WebElement email = driver.findElement(By.id("session_key"));
+        WebElement email = driver.findElement(By.id("session_key"));
         email.sendKeys("Clarusway selenium dersinden selamlar...");
-
+        driver.manage().window().maximize();
     }
+     @Test
+        public void nameLocator(){
+            WebElement password = driver.findElement(By.name("session_password"));
+            password.sendKeys("Sifre");
+
+        };
+
+
+
 }
