f2e3f350bda8cd8a72531b5096e65729b035dd52
QA Murat Tas <itmmt38@gmail.com>
2022-12-19 23:38:07 +0300

    Mondaylast

---
+++ src/test/java/Day05_LocatorPractice/C05_Cehckboxes.java
@@ -0,0 +1,12 @@
+package Day05_LocatorPractice;
+
+public class C05_Cehckboxes {
+    //    Go to URL: https://demoqa.com/
+//    Click on Elements.
+//    Click on Checkbox.
+//    Verify if Home checkbox is selected.
+//    Verify that "You have selected" is visible.
+
+
+
+}
