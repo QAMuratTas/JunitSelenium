cf50a71e834c1fdc084ec5851b1e1b60176026e0
QA Murat Tas <itmmt38@gmail.com>
2022-12-23 20:43:49 +0300

    LessonFri

--- src/test/java/Day07_Iframe_WindowHandle/Homework3.java
+++ src/test/java/Day07_Iframe_WindowHandle/Homework3.java
@@ -3,7 +3,6 @@ package Day07_Iframe_WindowHandle;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import utilities.BaseTest;
-
 import java.util.Iterator;
 import java.util.Set;
 
@@ -22,23 +21,14 @@ String homePage=driver.getWindowHandle();
         driver.findElement(By.xpath("//button[@id='windowButton']")).click();
         Set<String> w = driver.getWindowHandles();
         Iterator<String> i = w.iterator();
-
         while (i.hasNext()){
             driver.switchTo().window(i.next());
             if(driver.getWindowHandle().equals(homePage)){
                 continue;
             }
             System.out.println("Child Window = " + driver.findElement(By.id("sampleHeading")).getText());
-
-
         }
 driver.switchTo().window(homePage);
         System.out.println("HomePage Title= " + driver.getTitle());
-
-
     }
-
-
-
-
 }
---
+++ src/test/java/Day9_JSEScroll_Cookies_Files/C1_ScrollActions.java
@@ -0,0 +1,27 @@
+package Day9_JSEScroll_Cookies_Files;
+
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import utilities.BaseTest;
+
+public class C1_ScrollActions extends BaseTest {
+
+    @Test
+    public void scrollPageUpDown() throws InterruptedException {
+        driver.get("https://amazon.com");
+        Actions actions = new Actions(driver);
+        Thread.sleep(2000);
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        Thread.sleep(2000);
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        Thread.sleep(2000);
+        actions.sendKeys(Keys.PAGE_UP).perform();
+        Thread.sleep(2000);
+        actions.sendKeys(Keys.PAGE_UP).perform();
+
+    }
+
+
+
+}
