bbab497841dcb7232ed39818a35af1cd8c057b3c
QA Murat Tas <itmmt38@gmail.com>
2022-12-26 21:18:38 +0300

    monday

---
+++ src/test/java/Day9_JSEScroll_Cookies_Files/c8_fileExiat.java
@@ -0,0 +1,24 @@
+package Day9_JSEScroll_Cookies_Files;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+
+public class c8_fileExiat {
+    @Test
+    public void fileExist() {
+
+    //C:\\Users\\MURAT\\Desktop\\fileExist.png
+        System.out.println(System.getProperty("user.home"));
+        String homePath = System.getProperty("user.home");
+        String filePath = "\\Desktop\\fileExist.png";
+        String fullPath =homePath+filePath;
+        File image =new File(fullPath);
+        Assert.assertTrue(image.exists());
+
+
+    }
+
+
+}
---
+++ src/test/java/Day9_JSEScroll_Cookies_Files/c9_fileDownload.java
@@ -0,0 +1,12 @@
+package Day9_JSEScroll_Cookies_Files;
+
+import utilities.BaseTest;
+
+public class c9_fileDownload extends BaseTest {
+
+
+
+
+
+
+}
