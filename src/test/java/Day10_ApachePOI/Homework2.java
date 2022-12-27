05b3e2315629a51cd67f959725d10b4d2de34835
QA Murat Tas <itmmt38@gmail.com>
2022-12-27 14:26:06 +0300

    hw

--- src/test/java/Day10_ApachePOI/Homework2.java
+++ src/test/java/Day10_ApachePOI/Homework2.java
@@ -1,4 +1,31 @@
 package Day10_ApachePOI;
 
-public class Homework2 {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.BaseTest;
+
+import java.io.File;
+
+public class Homework2 extends BaseTest {
+
+    // Go to https://the-internet.herokuapp.com/download
+    //  Download sample.png file
+    //  Then verify if the file downloaded successfully
+
+
+    @Test
+    public void fileDownloadTest() throws InterruptedException {
+
+        driver.get("https://the-internet.herokuapp.com/download");
+        WebElement download = driver.findElement(By.linkText("sample.png"));
+        download.click();
+        Thread.sleep(5000);
+        String homePath = System.getProperty("user.home");
+        String filePath = "\\Downloads\\sample.png";
+        String fullPath =homePath+filePath;
+        File image =new File(fullPath);
+        Assert.assertTrue(image.exists());
+    }
 }
